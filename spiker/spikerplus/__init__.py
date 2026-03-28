6bb7911a2cfaf38bc608dbbd006114f782bdaeea
vlordier <vlordier@users.noreply.github.com>
2026-03-28 23:37:24 +0100

    refactor: clean up __init__.py exports
    - Consolidate imports for cleaner code
    - Add device module exports (get_device, to_device)
    - Add __all__ for explicit public API
    - Use cleaner multi-import statements

--- spiker/spikerplus/__init__.py
+++ spiker/spikerplus/__init__.py
@@ -12,70 +12,71 @@ This package provides:
 - Input validation and custom exceptions
 """
 
+from .device import get_device as get_device
+from .device import to_device as to_device
 from .exceptions import (
-    DataLoaderError as DataLoaderError,
+    DataLoaderError,
+    LayerConfigError,
+    NetworkConfigError,
+    NeuronModelError,
+    QuantizationError,
+    SpikerError,
+    TrainingError,
+    VHDLGenerationError,
 )
-from .exceptions import (
-    LayerConfigError as LayerConfigError,
-)
-from .exceptions import (
-    NetworkConfigError as NetworkConfigError,
-)
-from .exceptions import (
-    NeuronModelError as NeuronModelError,
-)
-from .exceptions import (
-    QuantizationError as QuantizationError,
-)
-from .exceptions import (
-    SpikerError as SpikerError,
-)
-from .exceptions import (
-    TrainingError as TrainingError,
-)
-from .exceptions import (
-    VHDLGenerationError as VHDLGenerationError,
-)
-from .net_builder import NetBuilder as NetBuilder
-from .optimizer import Optimizer as Optimizer
-from .trainer import Trainer as Trainer
+from .net_builder import NetBuilder
+from .optimizer import Optimizer
+from .trainer import Trainer
 from .types import (
-    LayerConfig as LayerConfig,
-)
-from .types import (
-    NetworkConfig as NetworkConfig,
-)
-from .types import (
-    NeuronModel as NeuronModel,
-)
-from .types import (
-    OptimizerConfig as OptimizerConfig,
-)
-from .types import (
-    QuantizationConfig as QuantizationConfig,
-)
-from .types import (
-    ReadoutType as ReadoutType,
-)
-from .types import (
-    ResetMechanism as ResetMechanism,
-)
-from .types import (
-    TrainingConfig as TrainingConfig,
+    LayerConfig,
+    NetworkConfig,
+    NeuronModel,
+    OptimizerConfig,
+    QuantizationConfig,
+    ReadoutType,
+    ResetMechanism,
+    TrainingConfig,
 )
 from .validation import (
-    validate_choice as validate_choice,
+    validate_choice,
+    validate_layer_config,
+    validate_net_config,
+    validate_range,
+    validate_type,
 )
-from .validation import (
-    validate_layer_config as validate_layer_config,
-)
-from .validation import (
-    validate_net_config as validate_net_config,
-)
-from .validation import (
-    validate_range as validate_range,
-)
-from .validation import (
-    validate_type as validate_type,
-)
-from .vhdl_generator import VhdlGenerator as VhdlGenerator
+from .vhdl_generator import VhdlGenerator
+
+__all__ = [
+    # Device
+    "get_device",
+    "to_device",
+    # Exceptions
+    "DataLoaderError",
+    "LayerConfigError",
+    "NetworkConfigError",
+    "NeuronModelError",
+    "QuantizationError",
+    "SpikerError",
+    "TrainingError",
+    "VHDLGenerationError",
+    # Core
+    "NetBuilder",
+    "Optimizer",
+    "Trainer",
+    "VhdlGenerator",
+    # Types
+    "LayerConfig",
+    "NetworkConfig",
+    "NeuronModel",
+    "OptimizerConfig",
+    "QuantizationConfig",
+    "ReadoutType",
+    "ResetMechanism",
+    "TrainingConfig",
+    # Validation
+    "validate_choice",
+    "validate_layer_config",
+    "validate_net_config",
+    "validate_range",
+    "validate_type",
+]
